578d188f10fe0c2d87e4ea09e11a9611783a56ad
Bastien <bastien_maurice@hotmail.com>
2019-11-11 20:28:57 +0100

    Ajout footer et header ; harmonisation du template DEEPLY

--- main.js
+++ main.js
@@ -1,18 +1,33 @@
 const { app, BrowserWindow } = require('electron')
 
+let win;
+let isToolsDev;
+const args = process.argv.slice(1);
+isToolsDev = args.some(val => val === '--devTools');
+
+
 function createWindow () {
-  let win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      nodeIntegration: true
-    },
-    frame: false,
-    titleBarStyle: 'hiden'
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+          nodeIntegration: true,
+        },
+        frame: false,
+        titleBarStyle: 'hidden',
+        icon: './src/assets/icon/icon_transparent.png',
+        show: false
+    });
 
-  })
+  win.loadURL('http://localhost:4200/');
 
-  win.loadURL('http://localhost:4200/')
+  win.once('ready-to-show', () => {
+    win.show();
+  });
+
+  if (isToolsDev) {
+    win.webContents.openDevTools();
+  }
 }
 
 app.on('ready', createWindow)
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,30 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule} from '@angular/material'
+import {MatButtonModule, MatIconModule, MatMenuModule, MatToolbarModule} from '@angular/material';
+import {HeaderComponent} from './core/header/header.component';
+import {FooterComponent} from './core/footer/footer.component';
+import {NgxElectronModule} from 'ngx-electron';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatToolbarModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        FooterComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatToolbarModule,
+        NgxElectronModule,
+        MatIconModule,
+        MatButtonModule,
+        MatMenuModule,
+        FlexLayoutModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
